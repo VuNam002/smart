6d5f883698a4fc7924104cfb1de09cbef09f3503
VuNam002 <namvu7702@gmail.com>
2025-07-09 14:57:17 +0700

    Cập nhật tính năng hiển thị theo danh mục sản phẩm

--- controllers/client/homecontroller.js
+++ controllers/client/homecontroller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/productmodel");
+const ProductCategory = require("../../models/product-category.model");
 const productHelper = require("../../helpers/product.helper");
+const categoryHelper = require("../../helpers/category.helper");
 
 module.exports.index = async (req, res) => {
   //Lấy ra sản phẩm nổi bật
@@ -7,15 +9,9 @@ module.exports.index = async (req, res) => {
     featured: "1",
     deleted: false,
     status: "active",
-  }).limit(8);//Lấy ra số lượng sản phẩm tối đa
+  }).limit(8); //Lấy ra số lượng sản phẩm tối đa
   const newProductsFeatured = productHelper.calcNewPrice(productsFeatured);
 
-  const products = await Product.find({
-    status: "active",
-    deleted: false,
-  }).sort({ position: "desc" });
-  const newProducts = productHelper.calcNewPrice(products);
-  
   //Lấy ra sản phẩm mới nhất
   const productsNew = await Product.find({
     status: "active",
@@ -23,11 +19,32 @@ module.exports.index = async (req, res) => {
   }).sort({ createdAt: "desc" }).limit(8);
   const newProductsNew = productHelper.calcNewPrice(productsNew);
 
+  // Lấy ra danh sách sản phẩm theo danh mục
+  const categories = await ProductCategory.find({
+    deleted: false,
+    status: "active",
+    parent_id: "",
+  });
+
+  for (const category of categories) {
+    const listSubCategory = await categoryHelper.getSubCategory(category.id);
+    const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+    const allCategoryIds = [category.id, ...listSubCategoryId];
+
+    const productsInCategory = await Product.find({
+      product_category_id: { $in: allCategoryIds },
+      deleted: false,
+      status: "active",
+    }).sort({ position: "desc" }).limit(8);
+
+    category.products = productHelper.calcNewPrice(productsInCategory);
+  }
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    products: newProducts,
     productsFeatured: newProductsFeatured,
     productsNew: newProductsNew,
+    categories: categories,
   });
 };
